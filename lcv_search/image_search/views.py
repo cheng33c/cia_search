0ac5804dc80a00c43c193025a859ebf18529eb68
cheng33c <asglitc12@gmail.com>
2017-09-24 08:05:11 +0800

    add:search

--- lcv_search/image_search/views.py
+++ lcv_search/image_search/views.py
@@ -1,11 +1,11 @@
 import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import View
-from image_search.models import ImageType
 from elasticsearch import Elasticsearch
-from datetime import datetime
 
+from image_search.models import ImageType
 
 client = Elasticsearch(hosts=['127.0.0.1'])
 
@@ -26,3 +26,41 @@ class SearchSuggest(View):
                 source = match._source
                 re_datas.append(source["tags"])
         return HttpResponse(json.dumps(re_datas), content_type="application/json")
+
+class SearchView(View):
+    def get(self, request):
+        key_words = request.GET.get("q", "")
+        response = client.search(
+                    index="baidu",
+                    body={
+                        "query": {
+                            "multi_match": {
+                                "query": "开心",
+                                "fields": ["tags"]
+                            }
+                        },
+                        "from": 0,
+                        "size": 10,
+                        "highlight": {
+                            "pre_tags": ['<span class="keyWord">'],
+                            "post_tags": ['</span>'],
+                            "fields": {}
+                        }
+                    }
+                )
+        total_nums = response["hits"]["total"]
+
+        hit_list = []
+        '''
+        for hit in response["hits"]["hits"]:
+            hit_dict = {}
+            if "tags" in hit["highlight"]:
+                hit_dict["tags"] = "".join(hit["highlight"]["tags"])
+            else:
+                hit_dict["tags"] = hit["_source"]["tags"]
+            hit_dict["url"] = hit["_source"]["url"]
+            hit_dict["score"] = hit["_score"]
+
+            hit_list.append(hit_dict)
+        '''
+        return render(request, "result.html", {"all_hits": hit_list, "key_words": key_words})
